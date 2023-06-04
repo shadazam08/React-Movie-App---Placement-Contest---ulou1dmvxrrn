a7c965f2e4bce1190d8ba8e6a6aef6d089d598b0
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-06-04 16:15:21 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
+import { searchMovieData } from "./api";
 
-const SearchBar = ({ onSearch }) => {
-  const [searchInput, setSearchInput] = useState("");
-  const handleInputChange = (event) => {
-    setSearchInput(event.target.value);
-  };
-  const handelSubmit = (event) => {
-    event.preventDefault();
-    onSearch(searchInput);
-  };
-
-  return (
-    <form onSubmit={handelSubmit}>
-      <input type="text" className="search-input" />
-      <button type="submit" className="search-btn">
-        Search
-      </button>
-    </form>
-  );
+const SearchBar = ({ setMovies }) => {
+    const [searchInput, setSearchInput] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        searchMovieData(searchInput).then((data) => {
+            let { results } = data;
+            console.log(results);
+            setMovies(results);
+        });
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                onChange={(e) => setSearchInput(e.target.value)}
+                value={searchInput}
+                type="text"
+                className="search-input"
+            />
+            <button type="submit" className="search-btn">
+                Search
+            </button>
+        </form>
+    );
 };
 
 export default SearchBar;

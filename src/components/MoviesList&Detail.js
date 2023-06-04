5960958d5d3160b01c27f37a204904062f6c983a
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-06-04 15:52:28 +0530

    Update MoviesList&Detail.js

--- src/components/MoviesList&Detail.js
+++ src/components/MoviesList&Detail.js
@@ -1,33 +1,42 @@
 import React, { useState, useEffect } from "react";
 
-const MoviesList = (props) => {
+const MoviesList = ({movies, onSort, onSelectMovie}) => {
     return (
         <ul>
-            <li>
-                <img src={movie.poster_path} alt="movie-alt" />
-                <section className="title-year">
-                    <h2 className="movie-title">Title</h2>
-                    <p className="movie-release-year">Release Year: Year</p>
-                </section>
-            </li>
+            {movies.map((movie) => {
+                return (
+                    <li key={movie.id} onClick={() => onSelectMovie(movie)}>
+                        <img src={movie.poster_path} alt="movie-alt" />
+                        <section className="title-year">
+                            <h2 className="movie-title">{movie.title}</h2>
+                            <p className="movie-release-year">Release Year: {movie.release_date}</p>
+                        </section>
+                    </li>
+                )
+            }) }
+            
         </ul>
     )
 }
 
-const MovieDetails = (props) => {
+const MovieDetails = ({movie, onClose}) => {
     const [movieDetails, setMovieDetails] = useState("");
     useEffect (() => {
-        cont fetchMoviesDetails = async () => {
+        const fetchMoviesDetails = async () => {
             try{
-                const responce = await fetch( `https://api.themoviedb.org/3/movie/${movie.id}?api_key=[api_key]&language=en-us`
-);
+                const responce = await fetch( `https://api.themoviedb.org/3/movie/${movie.id}?api_key=[api_key]&language=en-us`);
                 const data = await responce.json();
                 setMovieDetails(data);
             }catch(error){
                 console.log(error);
             }
-        }
-    })
+        };
+        fetchMoviesDetails();
+    }, [movie.id]);
+    if(!movieDetails){
+        return <div>Loading movies Detais...</div>
+    }
+
 
     return (
         <article className="movie-details">
@@ -35,9 +44,9 @@ const MovieDetails = (props) => {
                 <img src={movieDetails.poster_path} alt="movie-poster-alt" className="movie-img"/>
             </section>
             <section className="movie-detail-title-year-plot">
-                <h2 className="movie-title-year">Title (Release Date)</h2>
-                <p className="movie-plot">Overview</p>
-                <button className="close-btn">Close</button>
+                <h2 className="movie-title-year">{movieDetails.title} {movieDetails.release_date}</h2>
+                <p className="movie-plot">{movieDetails.overview}</p>
+                <button className="close-btn">{onClose}</button>
             </section>
         </article>
     )
